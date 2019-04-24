fe33fcd90b3a70752819acaa5e83afabdb6a0d28
myeongjun <leaderj1001@gmail.com>
2019-04-24 23:33:30 +0900

    [Update] relative part

--- attention_augmented_conv.py
+++ attention_augmented_conv.py
@@ -32,14 +32,15 @@ class AugmentedConv(nn.Module):
         # flat_q, flat_k, flat_v
         # (batch_size, Nh, height * width, dvh or dkh)
         # dvh = dv / Nh, dkh = dk / Nh
-        flat_q, flat_k, flat_v = self.compute_flat_qkv(x, self.dk, self.dv, self.Nh)
+        # q, k, v
+        # (batch_size, Nh, height, width, dv or dk)
+        flat_q, flat_k, flat_v, q, k, v = self.compute_flat_qkv(x, self.dk, self.dv, self.Nh)
         logits = torch.matmul(flat_q.transpose(2, 3), flat_k)
 
-        # WORK IN PROCESS
-        # if self.relative:
-        #     h_rel_logits, w_rel_logits = self.relative_logits(flat_q)
-        #     logits += h_rel_logits
-        #     logits += w_rel_logits
+        if self.relative:
+            h_rel_logits, w_rel_logits = self.relative_logits(q)
+            logits += h_rel_logits
+            logits += w_rel_logits
         weights = F.softmax(logits, dim=-1)
 
         # attn_out
@@ -65,7 +66,7 @@ class AugmentedConv(nn.Module):
         flat_q = torch.reshape(q, (N, Nh, dk // Nh, H * W))
         flat_k = torch.reshape(k, (N, Nh, dk // Nh, H * W))
         flat_v = torch.reshape(v, (N, Nh, dv // Nh, H * W))
-        return flat_q, flat_k, flat_v
+        return flat_q, flat_k, flat_v, q, k, v
 
     def split_heads_2d(self, x, Nh):
         batch, channels, height, width = x.size()
@@ -78,11 +79,44 @@ class AugmentedConv(nn.Module):
         ret_shape = (batch, Nh * dv, H, W)
         return torch.reshape(x, ret_shape)
 
-    # def relative_logits(self, q):
-    #    WORK IN PROCESS
+    def relative_logits(self, q):
+        B, Nh, dk, H, W = q.size()
+        q = torch.transpose(q, 2, 4)
 
+        key_rel_w = nn.Parameter(torch.randn((2 * W - 1, dk), requires_grad=True))
+        rel_logits_w = self.relative_logits_1d(q, key_rel_w, H, W, Nh, "w")
 
-tmp = torch.randn((1, 3, 16, 16))
-conv = AugmentedConv(3, 32, kernel_size=3, dk=4, dv=2, Nh=2, relative=False)
-result = conv(tmp)
-print(result.shape)
+        key_rel_h = nn.Parameter(torch.randn((2 * H - 1, dk), requires_grad=True))
+        rel_logits_h = self.relative_logits_1d(torch.transpose(q, 2, 3), key_rel_h, W, H, Nh, "h")
+
+        return rel_logits_h, rel_logits_w
+
+    def relative_logits_1d(self, q, rel_k, H, W, Nh, case):
+        rel_logits = torch.einsum('bhxyd,md->bhmxy', q, rel_k)
+        rel_logits = torch.reshape(rel_logits, (-1, Nh * H, W, 2 * W - 1))
+        rel_logits = self.rel_to_abs(rel_logits)
+
+        rel_logits = torch.reshape(rel_logits, (-1, Nh, H, W, W))
+        rel_logits = torch.unsqueeze(rel_logits, dim=3)
+        rel_logits = rel_logits.repeat((1, 1, 1, H, 1, 1))
+
+        if case is "w":
+            rel_logits = torch.transpose(rel_logits, 3, 4)
+        elif case is "h":
+            rel_logits = torch.transpose(rel_logits, 2, 4).transpose(4, 5).transpose(3, 5)
+        rel_logits = torch.reshape(rel_logits, (-1, Nh, H * W, H * W))
+        return rel_logits
+
+    def rel_to_abs(self, x):
+        B, Nh, L, _ = x.shape
+
+        col_pad = torch.zeros((B, Nh, L, 1))
+        x = torch.cat((x, col_pad), dim=3)
+
+        flat_x = torch.reshape(x, (B, Nh, L * 2 * L))
+        flat_pad = torch.zeros((B, Nh, L - 1))
+        flat_x_padded = torch.cat((flat_x, flat_pad), dim=2)
+
+        final_x = torch.reshape(flat_x_padded, (B, Nh, L + 1, 2 * L - 1))
+        final_x = final_x[:, :, :L, L - 1:]
+        return final_x
